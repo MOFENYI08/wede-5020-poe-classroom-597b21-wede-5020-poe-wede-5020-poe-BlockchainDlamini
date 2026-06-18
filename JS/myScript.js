a1da27ecea5bd4dd048487db1acb8b3b6915e9f6
MOFENYI08 <ST10514555@rcconnect.edu.za>
2026-06-18 19:12:09 +0200

    myScript.js
    My Java Script

---
+++ JS/myScript.js
@@ -0,0 +1,7 @@
+// Get the form
+const from = document.getElementById('bookingForm');
+const name = document.getElementById('Name');
+const surname= document.getElementById('Surname');
+const text = document.getElementById('Nameofhairstyle');
+const number = document.getElementById('BraidingDate');
+const number = document.getElementById('BraidingTime');
